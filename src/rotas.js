0bdffc3539f7bb4b1d4a4d5d4d6454ef6701cf6e
Lucas Viana <luks.viana@gmail.com>
2022-06-28 16:06:35 -0300

    testagem

--- src/rotas.js
+++ src/rotas.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const rotasTwitter = require('./controladores/rotasTwitter');
 
 const rotas = express();
 
+rotas.get('/api/:id', rotasTwitter)
 
 module.exports = rotas;
--- src/servicos/twitter.js
+++ src/servicos/twitter.js
@@ -1,10 +1,14 @@
 const axios = require('axios');
 
 const instaciaAxios = axios.create({
-    baseURL: 'https://api.twitter.com/1.1/search/tweets.json',
+    baseURL: 'https://api.twitter.com',
     params: {
         api_key: process.env.API_KEY,
         api_keysecret: process.env.API_SECRET
+    },
+    headers: {
+        Authorization: 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAAJOdeAEAAAAAGanq%2BtBY9sv4khJYfeftry3%2BsmM%3DAzlW4QJGgyfSKMgHN3TiJbWUvMMy7UDpyvh0Zgsn2W4tqPmAth'
     }
 })
+
 module.exports = instaciaAxios;
