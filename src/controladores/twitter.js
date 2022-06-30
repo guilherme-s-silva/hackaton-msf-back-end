e3a6774b88267a644f138a458707de911a051941
unknown <gui.silva22@gmail.com>
2022-06-30 18:29:26 -0300

    filtro dos tweets adicionado

--- src/controladores/twitter.js
+++ src/controladores/twitter.js
@@ -1,10 +1,11 @@
 const axios = require('axios').default;
 const knex = require('../conexao');
+const fs = require('fs/promises');
 
 const listarTweets = async (id) => {
 
     try {
-         const {data} = await axios.get(`https://api.twitter.com/2/lists/${id}/tweets?tweet.fields=author_id%2Clang%2Ccreated_at&max_results=15`, {
+         const {data} = await axios.get(`https://api.twitter.com/2/lists/${id}/tweets?tweet.fields=author_id%2Clang%2Ccreated_at&max_results=100`, {
             headers: { 
                 'Authorization': 'Bearer ' + process.env.TOKEN
             }
@@ -19,7 +20,7 @@ const listarTweets = async (id) => {
 
 const listarMembros = async (id) => {
     try {
-        const {data} = await axios.get(`https://api.twitter.com/2/lists/1541520243729858560/members?user.fields=id%2Cprofile_image_url%2Clocation%2Curl%2Cverified%2Cpublic_metrics&max_results=20`, {
+        const {data} = await axios.get(`https://api.twitter.com/2/lists/1541520243729858560/members?user.fields=id%2Cprofile_image_url%2Clocation%2Curl%2Cverified%2Cpublic_metrics&max_results=100`, {
             headers: { 
                 'Authorization': 'Bearer ' + process.env.TOKEN
             }
@@ -35,18 +36,35 @@ const teste = async (req, res) => {
     const {id} = req.params;
 
     try {
-        const palavrasChave = await knex('palavras_chave').select('chave');
-
-        const chave = palavrasChave.map(chave => {
-            return chave.chave;
-        })
-
-        console.log(chave);
+        const palavrasChave = JSON.parse(await fs.readFile('./src/categorias/chaves.json'));
 
         const tweets = await listarTweets(id);
         const membros = await listarMembros(id);
-        
-        return res.status(200).json({tweets, membros});
+
+        const tweetsCombinados = [];
+
+        for(const tweet of tweets.data) {
+            for(const membro of membros.data) {
+                let tweetTratado = {};
+
+                if (tweet.author_id === membro.id){
+                    tweetTratado = {...tweet, ...membro}
+                    tweetsCombinados.push(tweetTratado);
+                }
+            }
+        }
+
+        const tweetsTratados = [];
+
+        tweetsCombinados.forEach(tweet => {
+            for(const item of palavrasChave){
+                if (tweet.text.toLowerCase().includes(item.toLowerCase())){
+                    return tweetsTratados.push(tweet);
+                }
+            }
+        });
+
+        return res.status(200).json(tweetsTratados);
     } catch (error) {
         return res.status(400).json(error.message)
     }
