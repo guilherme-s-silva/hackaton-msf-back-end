d50f104d3dab564ee643bf55c08fe80168c0f39e
unknown <gui.silva22@gmail.com>
2022-07-01 11:18:50 -0300

    correção do nome da rota geral

--- src/rotas.js
+++ src/rotas.js
@@ -3,9 +3,6 @@ const twitterQuery = require('./controladores/twitter');
 
 const rotas = express();
 
-/* rotas.get('/tweets/:id', twitterQuery.listarTweets);
-rotas.get('/membros/:id', twitterQuery.listarMembros); */
-
-rotas.get('/teste/:id', twitterQuery);
+rotas.get('/geral/:id', twitterQuery);
 
 module.exports = rotas;
